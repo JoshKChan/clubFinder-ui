878da08f88cea4b801d87d524440801b87076d44
Bobin <bobin.chen@mail.utoronto.ca>
2018-12-30 16:04:40 -0500

    Moves state and handlers out of TopBar and into parent components.

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -2,24 +2,8 @@ import React, { Component } from 'react';
 import { Menu, Form, Button } from 'semantic-ui-react';
 
 export class TopBar extends Component {
-  constructor(props) {
-    super(props);
-    // State should also probably be passed in from the parent
-    this.state = {
-      search: '',
-      submittedSearch: ''
-    }
-  }
-
-  // Remember setState is async
-  // These handlers should probably be passed in from the parent
-  handleChange = (e) => {
-    this.setState({ search: e.target.value });
-  }
-
-  handleSubmit = () => {
-    this.setState({ submittedSearch: this.state.search });
-  }
+  // State and eventhandlers that manipulate the state are passed in from the
+  // parent component and are stored in props.
 
   // Render search bar, logo, and sign-in/sign-up or name 
   // Can add location setting later
@@ -31,9 +15,9 @@ export class TopBar extends Component {
             Logo
           </Menu.Item>
           <Menu.Item>
-            <Form onSubmit={this.handleSubmit}>
+            <Form onSubmit={this.props.handleSubmit}>
               <Form.Input placeholder='Search...' style={{ width: '400px' }} 
-                onChange={this.handleChange} />
+                onChange={this.props.handleChange} />
             </Form>
           </Menu.Item>
           <Menu.Item position='right'>
@@ -41,10 +25,6 @@ export class TopBar extends Component {
             <Button primary>Log in/Sign up</Button> 
           </Menu.Item>
         </Menu>
-        {/*
-        search: {this.state.search} </br>
-        submittedSearch: {this.state.submittedSearch}
-        */}
       </div>
     );
   }
--- src/pages/SearchResultsPage.js
+++ src/pages/SearchResultsPage.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
 import { SearchResults, TopBar } from '../components';
+import axios from 'axios';
 
 export class SearchResultsPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      search: '',
+      submittedSearch: '',
+      searchResults: []
+    }
+  }
+
+  handleSearchChange = (e) => {
+    this.setState({ search: e.target.value });
+  }
+
+  handleSearchSubmit = () => {
+    // axios call here
+    // set search and searchResults afterwards
+    this.setState({ submittedSearch: this.state.search });
+  }
   render() {
     return (
       <div className='searchResultsPage'>
-        <TopBar />
-        <SearchResults />
+        <TopBar handleChange={this.handleSearchChange} 
+          handleSubmit={this.handleSearchSubmit} 
+          state={this.state} />
+        search: {this.state.search} <br/>
+        submittedSearch: {this.state.submittedSearch}
+        <SearchResults list={this.state.searchResults} />
       </div>
     );
   }
