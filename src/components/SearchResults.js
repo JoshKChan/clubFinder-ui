7644a362caffd1f8556605aa7bb44522178c0bef
Bobin <bobin.chen@mail.utoronto.ca>
2018-12-29 23:29:48 -0500

    Creates a component that lists search results. Currently uses hardcoded data.

---
+++ src/components/SearchResults.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { List } from 'semantic-ui-react';
+
+export class SearchResults extends Component { 
+  constructor(props) {
+    super(props);
+    this.list = [
+      {
+        _id: 1,
+        name: "yolo",
+        location: "somewhere",
+        distance: 2
+      },
+      {
+        _id: 2,
+        name: "yoyo",
+        location: "nowhere",
+        distance: 3.5
+      }
+    ];
+  } 
+
+  // Takes a list of club and event objects and maps it to a list of xml
+  // elements 
+  ListOfResults = (props) => {
+    const searchResults = props.list.map((result) => 
+      <List.Item key={result._id}>
+        {undefined ? result.image : null}
+        <List.Content floated='right'>
+          {undefined ? result.distance : null}
+          {result.distance}km
+        </List.Content>
+        <List.Content>
+          <List.Header as='a' href="https://www.google.ca">{result.name}</List.Header>
+          <List.Description>
+            {result.location}
+          </List.Description>
+        </List.Content>
+      </List.Item>
+    );
+    return (
+      <List divided verticalAlign='middle'>
+        {searchResults}
+      </List>
+    );
+  }
+
+  // Use this.props.searchResults instead of this.list
+  render() {
+    return (
+      <div className="searchResults">
+        <this.ListOfResults list={this.list} />
+      </div>
+    );
+  }
+}
